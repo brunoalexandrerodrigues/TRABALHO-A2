bab1239f21b2926053f57529742b4685e06fd37f
brunoalexandrerodrigues <133767418+brunoalexandrerodrigues@users.noreply.github.com>
2023-06-02 09:12:35 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,4 +1,3 @@
-pip install streamlit requests zipfile pandas
 import streamlit as st
 import pandas as pd
 import requests
@@ -10,31 +9,21 @@ def baixaDeputados(idLegislatura):
     df = pd.DataFrame(deputados)
     return df
 
-def baixaPropostasGoverno():
-    url = 'https://cdn.tse.jus.br/estatistica/sead/odsele/proposta_governo/proposta_governo_2022_RJ.zip'
-    response = requests.get(url)
-    with open('proposta_governo_2022_RJ.zip', 'wb') as file:
-        file.write(response.content)
-    with zipfile.ZipFile('proposta_governo_2022_RJ.zip', 'r') as zip_ref:
-        zip_ref.extractall('proposta_governo_2022_RJ')
-
-def carregaPropostasGoverno():
-    csv_file = 'proposta_governo_2022_RJ/proposta_governo_2022_RJ.csv'
-    df = pd.read_csv(csv_file)
+def baixaProposicoesDeputado(idDeputado):
+    url = f'https://dadosabertos.camara.leg.br/api/v2/proposicoes?itens=100&autorId={idDeputado}&ordem=ASC&ordenarPor=id'
+    r = requests.get(url)
+    proposicoes = r.json()['dados']
+    df = pd.DataFrame(proposicoes)
     return df
 
-def getPropostasDeputado(df_propostas, id_deputado):
-    df_deputado = df_propostas[df_propostas['id_cand'] == id_deputado]
-    return df_deputado
-
-st.title('Lista de Deputados e Propostas de Governo')
+st.title('Lista de Deputados em Exercício')
 
 idLegislatura = 57  # Defina aqui o valor da legislatura desejada
 
-df_deputados = baixaDeputados(idLegislatura)
+df = baixaDeputados(idLegislatura)
 
-st.header('Lista de Deputados do Rio de Janeiro')
-df_rio_de_janeiro = df_deputados[df_deputados['siglaUf'] == 'RJ']  # Filtra apenas os deputados do Rio de Janeiro
+st.header('Lista de deputados do Rio de Janeiro')
+df_rio_de_janeiro = df[df['siglaUf'] == 'RJ']  # Filtra apenas os deputados do Rio de Janeiro
 
 selected_deputado = st.selectbox('Selecione um deputado:', df_rio_de_janeiro['nome'])
 
@@ -50,19 +39,7 @@ if not selected_deputado_info.empty:
     st.write('ID: ' + str(selected_deputado_info['id']))
     st.write('Email: ' + str(selected_deputado_info['email']))
 
-    st.header('Propostas de Governo do Deputado')
-
-    # Baixa e carrega as propostas de governo
-    baixaPropostasGoverno()
-    df_propostas_governo = carregaPropostasGoverno()
-
-    # Filtra as propostas de governo pelo ID do deputado selecionado
-    df_propostas_deputado = getPropostasDeputado(df_propostas_governo, selected_deputado_info['id'])
-
-    if not df_propostas_deputado.empty:
-        st.dataframe(df_propostas_deputado)
-    else:
-        st.write('Nenhuma proposta de governo encontrada para o deputado selecionado.')
-else:
-    st.write('Nenhum deputado selecionado.')
-
+    st.header('Lista de Proposições do Deputado')
+    id_deputado = selected_deputado_info['id']
+    df_proposicoes = baixaProposicoesDeputado(id_deputado)
+    st.dataframe(df_proposicoes[['id', 'ementa']])
