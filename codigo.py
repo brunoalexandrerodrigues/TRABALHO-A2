5ca221aaec87a4e499f1ce95098aaa90a7ae2f65
brunoalexandrerodrigues <133767418+brunoalexandrerodrigues@users.noreply.github.com>
2023-06-02 08:57:49 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,8 +1,7 @@
 pip install streamlit requests zipfile pandas
 import streamlit as st
-import requests
-import zipfile
 import pandas as pd
+import requests
 
 def baixaDeputados(idLegislatura):
     url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?idLegislatura=' + str(idLegislatura)
@@ -11,29 +10,31 @@ def baixaDeputados(idLegislatura):
     df = pd.DataFrame(deputados)
     return df
 
-def baixaProposicoesDeputado(idDeputado):
-    url = f'https://dadosabertos.camara.leg.br/api/v2/proposicoes?itens=100&autorId={idDeputado}&ordem=ASC&ordenarPor=id'
-    r = requests.get(url)
-    proposicoes = r.json()['dados']
-    df = pd.DataFrame(proposicoes)
+def baixaPropostasGoverno():
+    url = 'https://cdn.tse.jus.br/estatistica/sead/odsele/proposta_governo/proposta_governo_2022_RJ.zip'
+    response = requests.get(url)
+    with open('proposta_governo_2022_RJ.zip', 'wb') as file:
+        file.write(response.content)
+    with zipfile.ZipFile('proposta_governo_2022_RJ.zip', 'r') as zip_ref:
+        zip_ref.extractall('proposta_governo_2022_RJ')
+
+def carregaPropostasGoverno():
+    csv_file = 'proposta_governo_2022_RJ/proposta_governo_2022_RJ.csv'
+    df = pd.read_csv(csv_file)
     return df
 
-# Função para fazer o download do arquivo ZIP
-def download_file(url, save_as):
-    response = requests.get(url, stream=True)
-    response.raise_for_status()
-    with open(save_as, 'wb') as file:
-        for chunk in response.iter_content(chunk_size=8192):
-            file.write(chunk)
+def getPropostasDeputado(df_propostas, id_deputado):
+    df_deputado = df_propostas[df_propostas['id_cand'] == id_deputado]
+    return df_deputado
 
 st.title('Lista de Deputados e Propostas de Governo')
 
 idLegislatura = 57  # Defina aqui o valor da legislatura desejada
 
-df = baixaDeputados(idLegislatura)
+df_deputados = baixaDeputados(idLegislatura)
 
-st.header('Lista de deputados do Rio de Janeiro')
-df_rio_de_janeiro = df[df['siglaUf'] == 'RJ']  # Filtra apenas os deputados do Rio de Janeiro
+st.header('Lista de Deputados do Rio de Janeiro')
+df_rio_de_janeiro = df_deputados[df_deputados['siglaUf'] == 'RJ']  # Filtra apenas os deputados do Rio de Janeiro
 
 selected_deputado = st.selectbox('Selecione um deputado:', df_rio_de_janeiro['nome'])
 
@@ -49,32 +50,19 @@ if not selected_deputado_info.empty:
     st.write('ID: ' + str(selected_deputado_info['id']))
     st.write('Email: ' + str(selected_deputado_info['email']))
 
-    st.header('Propostas de Governo do Rio de Janeiro')
+    st.header('Propostas de Governo do Deputado')
 
-    # Definir URL do arquivo ZIP das propostas de governo do Rio de Janeiro
-    url = 'https://cdn.tse.jus.br/estatistica/sead/odsele/proposta_governo/proposta_governo_2022_RJ.zip'
-    zip_filename = 'proposta_governo_2022_RJ.zip'
+    # Baixa e carrega as propostas de governo
+    baixaPropostasGoverno()
+    df_propostas_governo = carregaPropostasGoverno()
 
-    # Fazer o download do arquivo ZIP
-    st.write('Baixando arquivo ZIP das propostas de governo...')
-    download_file(url, zip_filename)
-    st.write('Download concluído!')
+    # Filtra as propostas de governo pelo ID do deputado selecionado
+    df_propostas_deputado = getPropostasDeputado(df_propostas_governo, selected_deputado_info['id'])
 
-    # Extrair o conteúdo do arquivo ZIP
-    with zipfile.ZipFile(zip_filename, 'r') as zip_ref:
-        st.write('Extraindo o arquivo ZIP das propostas de governo...')
-        zip_ref.extractall('proposta_governo_2022_RJ')
-        st.write('Extração concluída!')
-
-    # Ler os dados do arquivo CSV extraído
-    csv_filename = 'proposta_governo_2022_RJ/proposta_governo_2022_RJ.csv'
-    df_propostas = pd.read_csv(csv_filename)
-
-    # Filtrar as propostas de governo pelo ID do deputado selecionado
-    df_propostas_deputado = df_propostas[df_propostas['id_cand'] == selected_deputado_info['id']]
-
-    # Exibir os dados das propostas de governo
-    st.dataframe(df_propostas_deputado)
+    if not df_propostas_deputado.empty:
+        st.dataframe(df_propostas_deputado)
+    else:
+        st.write('Nenhuma proposta de governo encontrada para o deputado selecionado.')
 else:
-    st.write(':no_entry_sign: Sem informações disponíveis para o deputado selecionado!')
+    st.write('Nenhum deputado selecionado.')
 
