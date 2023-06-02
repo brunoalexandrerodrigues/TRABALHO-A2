ef17fea14826eebccac58f248583f997b7c7afb6
brunoalexandrerodrigues <133767418+brunoalexandrerodrigues@users.noreply.github.com>
2023-06-02 08:05:34 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,27 +2,6 @@ import streamlit as st
 import pandas as pd
 import requests
 
-# Definir estilo da página
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: url('https://www.rio.rj.gov.br/igstatic/50/13/00/5013008.jpg');
-        background-size: cover;
-    }
-    .title {
-        color: #ffffff;
-    }
-    .highlight {
-        background-color: #ff9900;
-        padding: 10px;
-        border-radius: 5px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 def baixaDeputados(idLegislatura):
     url = 'https://dadosabertos.camara.leg.br/api/v2/deputados?idLegislatura=' + str(idLegislatura)
     r = requests.get(url)
@@ -37,37 +16,42 @@ def baixaPropostasDeputado(idDeputado):
     df = pd.DataFrame(propostas)
     return df
 
-# Título com estilo
-st.markdown("<h1 style='text-align: center;'>Lista de Deputados do Rio de Janeiro</h1>", unsafe_allow_html=True)
+st.title('Lista de Deputados em Exercício')
+st.markdown('<h1 style="text-align: center;">Lista de Deputados em Exercício</h1>', unsafe_allow_html=True)
 
-idLegislatura = 57  # Considerando a legislatura atual
-df_deputados = baixaDeputados(idLegislatura)
+idLegislatura = st.slider('Escolha de qual legislatura você quer a lista de deputados', 50, 57, 57)
 
-# Filtra a lista apenas para o estado do Rio de Janeiro
-df_rio_de_janeiro = df_deputados[df_deputados['siglaUf'] == 'RJ']
+df = baixaDeputados(idLegislatura)
 
-# Exibe a lista de deputados do Rio de Janeiro com nome e ID
-st.header('Lista de Deputados do Rio de Janeiro')
-df_display = df_rio_de_janeiro[['nome', 'id']]
-selected_row = st.radio('Selecione um deputado:', df_display['nome'], index=0)
-selected_deputado = df_display[df_display['nome'] == selected_row]
+st.header('Lista de deputados')
+with st.expander('Mostrar deputados'):
+    st.dataframe(df[['nome', 'id']])
 
-# Obtém as informações detalhadas do deputado selecionado
-deputado_id = selected_deputado['id'].values[0]
-selected_deputado_info = df_rio_de_janeiro[df_rio_de_janeiro['id'] == deputado_id]
+st.download_button('Baixar lista de deputados', data=df.to_csv(), file_name='deputados.csv', mime='text/csv')
 
-# Exibe detalhes adicionais do deputado selecionado com destaque
+st.header('Gráficos')
+st.subheader('Número de deputados por partido')
+st.bar_chart(df['siglaPartido'].value_counts())
+st.subheader('Número de deputados por estado')
+st.bar_chart(df['siglaUf'].value_counts())
+
+st.header('Lista de deputados do Rio de Janeiro')
+df_rio_de_janeiro = df[df['siglaUf'] == 'RJ']
+selected_deputado = st.selectbox('Selecione um deputado:', df_rio_de_janeiro['nome'])
+selected_deputado_info = df_rio_de_janeiro[df_rio_de_janeiro['nome'] == selected_deputado]
+
 if not selected_deputado_info.empty:
     selected_deputado_info = selected_deputado_info.iloc[0]
-    st.header('Detalhes do Deputado')
-    st.markdown('<div class="highlight"><h3>{}</h3></div>'.format(selected_deputado_info['nome']), unsafe_allow_html=True)
-    st.subheader('Foto:')
-    st.image(selected_deputado_info['urlFoto'], width=200)
-    st.subheader('Informações Pessoais:')
+    st.markdown('<h2 style="background-color: #ff9900; padding: 10px; border-radius: 5px; color: #ffffff;">Detalhes do Deputado</h2>', unsafe_allow_html=True)
+    st.image(selected_deputado_info['urlFoto'], width=130)
+    st.write('Nome: ' + selected_deputado_info['nome'])
+    st.write('Partido: ' + selected_deputado_info['siglaPartido'])
+    st.write('UF: ' + selected_deputado_info['siglaUf'])
     st.write('ID: ' + str(selected_deputado_info['id']))
     st.write('Email: ' + str(selected_deputado_info['email']))
-    st.write('Data de Nascimento: ' + str(selected_deputado_info['dataNascimento']))
-    st.write('Escolaridade: ' + str(selected_deputado_info['escolaridade']))
-    st.write('Rede Social: ' + str(selected_deputado_info['redeSocial']))
 
+st.header('Lista de Propostas de Pautas de Governo')
+id_deputado = selected_deputado_info['id']
+df_propostas = baixaPropostasDeputado(id_deputado)
+st.dataframe(df_propostas[['id', 'siglaTipo', 'ementa']])
 
