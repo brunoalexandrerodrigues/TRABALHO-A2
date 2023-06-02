ce44cb24f6ed4eb0286767f640b54d809da6aa64
brunoalexandrerodrigues <133767418+brunoalexandrerodrigues@users.noreply.github.com>
2023-06-02 09:14:27 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -16,17 +16,26 @@ def baixaProposicoesDeputado(idDeputado):
     df = pd.DataFrame(proposicoes)
     return df
 
-st.title('Lista de Deputados em Exercício')
+def baixaPautasGoverno():
+    url = 'https://cdn.tse.jus.br/estatistica/sead/odsele/proposta_governo/proposta_governo_2022_RJ.zip'
+    r = requests.get(url)
+    with open('proposta_governo_2022_RJ.zip', 'wb') as f:
+        f.write(r.content)
+    st.success('Arquivo baixado com sucesso.')
 
+# Título e seleção da legislatura
+st.title('Lista de Deputados em Exercício')
 idLegislatura = 57  # Defina aqui o valor da legislatura desejada
 
 df = baixaDeputados(idLegislatura)
 
-st.header('Lista de deputados do Rio de Janeiro')
-df_rio_de_janeiro = df[df['siglaUf'] == 'RJ']  # Filtra apenas os deputados do Rio de Janeiro
+# Filtrar deputados do Rio de Janeiro
+df_rio_de_janeiro = df[df['siglaUf'] == 'RJ']
 
+# Seleção do deputado
 selected_deputado = st.selectbox('Selecione um deputado:', df_rio_de_janeiro['nome'])
 
+# Informações do deputado selecionado
 selected_deputado_info = df_rio_de_janeiro[df_rio_de_janeiro['nome'] == selected_deputado]
 
 if not selected_deputado_info.empty:
@@ -39,6 +48,11 @@ if not selected_deputado_info.empty:
     st.write('ID: ' + str(selected_deputado_info['id']))
     st.write('Email: ' + str(selected_deputado_info['email']))
 
+    # Botão para baixar as pautas de governo
+    if st.button('Baixar Pautas de Governo'):
+        baixaPautasGoverno()
+
+    # Lista de proposições do deputado
     st.header('Lista de Proposições do Deputado')
     id_deputado = selected_deputado_info['id']
     df_proposicoes = baixaProposicoesDeputado(id_deputado)
