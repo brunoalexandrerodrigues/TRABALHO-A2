ccf97536df6e8296294818f561c87cdce48770c5
brunoalexandrerodrigues <133767418+brunoalexandrerodrigues@users.noreply.github.com>
2023-06-05 12:48:16 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -14,13 +14,9 @@ def baixaProposicoes():
     proposicoes = r.json()
     return proposicoes
 
-def baixaDeputados():
-    url = "https://dadosabertos.camara.leg.br/api/v2/deputados"
-    params = {
-        "siglaUf": "RJ",
-        "itens": 100
-    }
-    r = requests.get(url, params=params)
+def baixaDeputados(idLegislatura):
+    url = f"https://dadosabertos.camara.leg.br/api/v2/deputados?idLegislatura={idLegislatura}"
+    r = requests.get(url)
     deputados = r.json()["dados"]
     df = pd.DataFrame(deputados)
     return df
@@ -29,26 +25,25 @@ st.title("Lista de Deputados do Rio de Janeiro e suas Ementas")
 
 autores_proposicoes = baixaAutoresProposicoes()
 proposicoes = baixaProposicoes()
-deputados = baixaDeputados()
+deputados = baixaDeputados(57)
 
-deputados_rj_autores = []
-for proposta in proposicoes:
-    if proposta.get("siglaUFAutor") == "RJ":
-        id_proposicao = proposta.get("id")
-        autores_proposicao = [autor for autor in autores_proposicoes if autor.get("idProposicao") == id_proposicao]
-        for autor in autores_proposicao:
-            id_autor = autor.get("idAutor")
-            deputado_rj = deputados[deputados["id"] == id_autor]
-            if not deputado_rj.empty:
-                deputados_rj_autores.append(deputado_rj.iloc[0])
+autores_proposicoes_rj = []
+for autor in autores_proposicoes:
+    if autor.get("dados", {}).get("siglaUFAutor") == "RJ":
+        id_autor = autor.get("idAutor")
+        deputado_rj = deputados[deputados["id"] == id_autor]
+        if not deputado_rj.empty:
+            autores_proposicoes_rj.append(autor)
 
-df_deputados_rj_autores = pd.DataFrame(deputados_rj_autores)
+df_autores_proposicoes_rj = pd.DataFrame(autores_proposicoes_rj)
 df_proposicoes = pd.DataFrame(proposicoes)
 
-df_final = pd.merge(df_deputados_rj_autores, df_proposicoes, left_on="id", right_on="id", how="inner")
+df_deputados_rj_autores = pd.merge(df_autores_proposicoes_rj, deputados, left_on="idAutor", right_on="id", how="inner")
+
+df_final = pd.merge(df_deputados_rj_autores, df_proposicoes, left_on="idProposicao", right_on="id", how="inner")
 
 for index, row in df_final.iterrows():
-    st.markdown(f"## {row['nome']} ({row['siglaPartido']})")
+    st.markdown(f"## {row['nomeAutor']} ({row['siglaPartidoAutor']})")
     st.image(row['urlFoto'], width=200)
     st.write(f"**Ementa:** {row['ementa']}")
     st.markdown("---")
