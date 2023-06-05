dc24f18cf8472309d5e68140f922c3bc12d4658e
brunoalexandrerodrigues <133767418+brunoalexandrerodrigues@users.noreply.github.com>
2023-06-05 10:33:04 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,44 +1,55 @@
+pip install streamlit pandas requests 
 import streamlit as st
+import pandas as pd
 import requests
 
-def get_proposicoes(dataInicio=None, dataFim=None, id=None, ano=None, dataApresentacaoInicio=None, dataApresentacaoFim=None, idAutor=None, autor=None):
-    url = "https://dadosabertos.camara.leg.br/api/v2/proposicoes"
-    params = {
-        "dataInicio": dataInicio,
-        "dataFim": dataFim,
-        "id": id,
-        "ano": ano,
-        "dataApresentacaoInicio": dataApresentacaoInicio,
-        "dataApresentacaoFim": dataApresentacaoFim,
-        "idAutor": idAutor,
-        "autor": autor
-    }
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        return response.json()["dados"]
-    else:
-        return []
+def baixaDeputados(idLegislatura):
+    url = f"https://dadosabertos.camara.leg.br/api/v2/deputados?idLegislatura={idLegislatura}"
+    r = requests.get(url)
+    deputados = r.json()["dados"]
+    df = pd.DataFrame(deputados)
+    return df
 
-def main():
-    st.title("Consulta de Proposições na Câmara dos Deputados")
-    st.write("Este aplicativo consulta as proposições na Câmara dos Deputados.")
+def baixaProposicoesDeputado(idDeputado):
+    url = f"https://dadosabertos.camara.leg.br/api/v2/proposicoes?itens=100&autorId={idDeputado}&ordem=ASC&ordenarPor=id"
+    r = requests.get(url)
+    proposicoes = []
+    if r.status_code == 200:
+        try:
+            proposicoes = r.json()["dados"]
+        except KeyError:
+            pass
+    return proposicoes
 
-    dataInicio = st.text_input("Data de Início (formato: AAAA-MM-DD)")
-    dataFim = st.text_input("Data de Fim (formato: AAAA-MM-DD)")
-    idAutor = st.text_input("ID do Autor")
-    autor = st.text_input("Nome do Autor")
-    
-    if st.button("Buscar Proposições"):
-        proposicoes = get_proposicoes(dataInicio=dataInicio, dataFim=dataFim, idAutor=idAutor, autor=autor)
-        
-        if proposicoes:
-            st.write(f"Foram encontradas {len(proposicoes)} proposições:")
-            for proposta in proposicoes:
-                st.write(f"ID: {proposta['id']}")
-                st.write(f"Ementa: {proposta['ementa']}")
-                st.write("-----")
-        else:
-            st.write("Nenhuma proposição encontrada.")
+st.title("Lista de Deputados em Exercício")
 
-if __name__ == "__main__":
-    main()
+idLegislatura = 57  # Defina aqui o valor da legislatura desejada
+
+df = baixaDeputados(idLegislatura)
+
+st.header("Lista de deputados do Rio de Janeiro")
+df_rio_de_janeiro = df[df["siglaUf"] == "RJ"]  # Filtra apenas os deputados do Rio de Janeiro
+
+selected_deputado = st.selectbox("Selecione um deputado:", df_rio_de_janeiro["nome"])
+
+selected_deputado_info = df_rio_de_janeiro[df_rio_de_janeiro["nome"] == selected_deputado]
+
+if not selected_deputado_info.empty:
+    selected_deputado_info = selected_deputado_info.iloc[0]
+    st.markdown(
+        '<h2 style="background-color: #ff9900; padding: 10px; border-radius: 5px; color: #ffffff;">Detalhes do Deputado</h2>',
+        unsafe_allow_html=True,
+    )
+    st.image(selected_deputado_info["urlFoto"], width=130)
+    st.write("Nome: " + selected_deputado_info["nome"])
+    st.write("Partido: " + selected_deputado_info["siglaPartido"])
+    st.write("UF: " + selected_deputado_info["siglaUf"])
+    st.write("ID: " + str(selected_deputado_info["id"]))
+    st.write("Email: " + str(selected_deputado_info["email"]))
+
+    st.header("Lista de Proposições do Deputado")
+    proposicoes = baixaProposicoesDeputado(selected_deputado_info["id"])
+    for proposta in proposicoes:
+        st.write("ID: ", proposta["id"])
+        st.write("Ementa: ", proposta["ementa"])
+        st.markdown("---")
