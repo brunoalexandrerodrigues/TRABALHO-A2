50d76496a1888337c47aa4a563450a26ed17d29f
brunoalexandrerodrigues <133767418+brunoalexandrerodrigues@users.noreply.github.com>
2023-06-06 09:01:24 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,42 +1,62 @@
 import streamlit as st
 import requests
 
-def scrape_data(url):
+def scrape_deputies(url):
     response = requests.get(url)
     data = response.json()
+    deputies = []
 
-    results = []
     for item in data['dados']:
-        descricao_tipo = item.get('descricaoTipo', '')
-        ementa = item.get('ementa', '')
-        ementa_detalhada = item.get('ementaDetalhada', '')
-        keywords = item.get('keywords', [])
+        if item['siglaUf'] == 'RJ':
+            deputy = {
+                'nome': item['nome'],
+                'id': item['id']
+            }
+            deputies.append(deputy)
 
-        result = {
-            'descricaoTipo': descricao_tipo,
-            'ementa': ementa,
-            'ementaDetalhada': ementa_detalhada,
-            'keywords': keywords
-        }
-        results.append(result)
+    return deputies
 
-    return results
+def scrape_proposals(url):
+    response = requests.get(url)
+    data = response.json()
+    proposals = []
+
+    for item in data['dados']:
+        if item['siglaUf'] == 'RJ':
+            proposal = {
+                'descricaoTipo': item.get('descricaoTipo', ''),
+                'ementa': item.get('ementa', ''),
+                'ementaDetalhada': item.get('ementaDetalhada', ''),
+                'keywords': item.get('keywords', [])
+            }
+            proposals.append(proposal)
+
+    return proposals
 
 # Configurações da aplicação Streamlit
-st.title("Raspagem de Dados das Proposições")
+st.title("Informações dos Deputados e Proposições")
 
 # Raspagem dos dados
-url_autores = 'https://dadosabertos.camara.leg.br/arquivos/proposicoesAutores/json/proposicoesAutores-2023.json'
-url_proposicoes = 'https://dadosabertos.camara.leg.br/arquivos/proposicoes/json/proposicoes-2023.json'
+url_deputies = 'https://dadosabertos.camara.leg.br/api/v2/deputados'
+url_proposals = 'https://dadosabertos.camara.leg.br/arquivos/proposicoes/json/proposicoes-2023.json'
 
-autores = scrape_data(url_autores)
-proposicoes = scrape_data(url_proposicoes)
+deputies = scrape_deputies(url_deputies)
+proposals = scrape_proposals(url_proposals)
 
-# Exibição dos resultados no Streamlit
-st.header("Proposições Autores")
-for autor in autores:
-    st.write(autor)
+# Exibição dos deputados
+st.header("Deputados do RJ")
+selected_deputy = st.selectbox("Selecione um deputado", [deputy['nome'] for deputy in deputies])
+deputy_id = next((deputy['id'] for deputy in deputies if deputy['nome'] == selected_deputy), None)
 
-st.header("Proposições")
-for proposicao in proposicoes:
-    st.write(proposicao)
+if deputy_id:
+    st.subheader("Proposições do Deputado")
+    deputy_proposals = [proposal for proposal in proposals if proposal['id'] == deputy_id]
+
+    if deputy_proposals:
+        for proposal in deputy_proposals:
+            st.write(proposal)
+    else:
+        st.write("Nenhuma proposição encontrada para o deputado selecionado.")
+else:
+    st.write("Nenhum deputado selecionado.")
+
