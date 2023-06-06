691f7813377f85603d3fa0d9d4431f490107ef71
brunoalexandrerodrigues <133767418+brunoalexandrerodrigues@users.noreply.github.com>
2023-06-06 08:55:23 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,56 +1,36 @@
 import streamlit as st
 import requests
 
-def scrape_proposicoes_autores():
-    url = 'https://dadosabertos.camara.leg.br/arquivos/proposicoesAutores/json/proposicoesAutores-2023.json'
+def scrape_data(url):
     response = requests.get(url)
     data = response.json()
 
-    autores = []
+    results = []
     for item in data['dados']:
         descricao_tipo = item.get('descricaoTipo', '')
         ementa = item.get('ementa', '')
         ementa_detalhada = item.get('ementaDetalhada', '')
         keywords = item.get('keywords', [])
 
-        autor = {
+        result = {
             'descricaoTipo': descricao_tipo,
             'ementa': ementa,
             'ementaDetalhada': ementa_detalhada,
             'keywords': keywords
         }
-        autores.append(autor)
+        results.append(result)
 
-    return autores
-
-def scrape_proposicoes():
-    url = 'https://dadosabertos.camara.leg.br/arquivos/proposicoes/json/proposicoes-2023.json'
-    response = requests.get(url)
-    data = response.json()
-
-    proposicoes = []
-    for item in data['dados']:
-        descricao_tipo = item.get('descricaoTipo', '')
-        ementa = item.get('ementa', '')
-        ementa_detalhada = item.get('ementaDetalhada', '')
-        keywords = item.get('keywords', [])
-
-        proposicao = {
-            'descricaoTipo': descricao_tipo,
-            'ementa': ementa,
-            'ementaDetalhada': ementa_detalhada,
-            'keywords': keywords
-        }
-        proposicoes.append(proposicao)
-
-    return proposicoes
+    return results
 
 # Configurações da aplicação Streamlit
 st.title("Raspagem de Dados das Proposições")
 
 # Raspagem dos dados
-autores = scrape_proposicoes_autores()
-proposicoes = scrape_proposicoes()
+url_autores = 'https://dadosabertos.camara.leg.br/arquivos/proposicoesAutores/json/proposicoesAutores-2023.json'
+url_proposicoes = 'https://dadosabertos.camara.leg.br/arquivos/proposicoes/json/proposicoes-2023.json'
+
+autores = scrape_data(url_autores)
+proposicoes = scrape_data(url_proposicoes)
 
 # Exibição dos resultados no Streamlit
 st.header("Proposições Autores")
